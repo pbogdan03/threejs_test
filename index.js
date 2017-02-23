b2d16fd0f745b4bc1281555fc57806db134d0c4b
bogdanp <bogdan.pestritu@rga.com>
2017-02-23 16:50:00 +0200

    how to maximize framerate?

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
 (function() {
-  var AnimationApp = function(settings) {
-    this.settings = {
-      width: settings.width || window.innerWidth,
-      height: settings.height || window.innerHeight,
-      verticalMin: settings.verticalMin || -15,
-      verticalMax: settings.verticalMax || 15,
-      horizontalMin: settings.horizontalMin || -500,
-      horizontalMax: settings.horizontalMax || 500,
-      step: settings.step || 1,
-      lineSubdivs: settings.lineSubdivs || 6
-    };
-    this.resolution = new THREE.Vector2(settings.width, settings.height);
-    this.theta = 0;
-    this.windowHalfX = this.settings.width / 2;
-    this.windowHalfY = this.settings.height / 2;
-    this.mouseX = 0;
-    this.mouseY = 0;
-    this.noise = noise;
-    this.time = Date.now();
+  var width = window.innerWidth,
+      height = window.innerHeight,
+      verticalMin = -15, // y start point of line
+      verticalMax = 15, // y end point of line
+      horizontalMin = -500, // x start point of line
+      horizontalMax = 500, // x end point of line
+      step = 1,
+      lineSubdivs = 6,
+      resolution = new THREE.Vector2(width, height),
+      theta = 0,
+      windowHalfX = width / 2,
+      windowHalfY = height / 2,
+      mouseX = 0,
+      mouseY = 0,
+      time = Date.now(),
+      camera, renderer,
+      gui, verticalMinCtrl, verticalMaxCtrl, horizontalMinCtrl, horizontalMaxCtrl, stepCtrl,
+      i, j, k, l, m, n, o, p;
 
-    this.init();
+  var settings = {
+    verticalMin: -15,
+    verticalMax: 15,
+    horizontalMin: -500,
+    horizontalMax: 500,
+    step: 1
   };
 
-  AnimationApp.prototype.init = function() {
+  init();
+
+  function init() {
     // Add DOM Stuff
     var el = document.querySelector('#root');
     var container = document.createElement( 'div' );
     el.appendChild( container );
 
     // Setup this.camera
-    this.camera = new THREE.PerspectiveCamera(
+    camera = new THREE.PerspectiveCamera(
       33,
-      this.settings.width / this.settings.height,
+      width / height,
       1,
       10000
     );
-    this.camera.position.z = 700;
+    camera.position.z = 700;
 
     // Setup this.scene and this.renderer
-    this.scene = new THREE.Scene();
-    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-    this.renderer.setPixelRatio( window.devicePixelRatio );
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
-    this.renderer.autoClear = false;
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( width, height );
+    renderer.autoClear = false;
 
     // Add the canvas to the DOM
-    container.appendChild( this.renderer.domElement );
+    container.appendChild( renderer.domElement );
 
     // for(var i = 0; i < this.scene.children.length; i++) {
     //   var points = this.scene.children[i].geometry.attributes.position.array;
@@ -61,46 +67,44 @@
     // console.log(this.scene.children[0].geometry.attributes.position);
 
     // Setup noise seed
-    this.noise.seed(Math.random());
+    noise.seed(Math.random());
 
-    this.addGUI();
-    this.animate();
-    this.addEvents();
+    // addGUI();
+    animate();
+    addEvents();
   }
 
-  AnimationApp.prototype.addGUI = function() {
-    this.gui = new dat.GUI();
+  function addGUI() {
+    gui = new dat.GUI();
 
-    this.verticalMinCtrl = this.gui.add(this.settings, "verticalMin", -500, 500).step(this.settings.step);
-    this.verticalMaxCtrl = this.gui.add(this.settings, "verticalMax", -500, 500).step(this.settings.step);
-    this.horizontalMinCtrl = this.gui.add(this.settings, "horizontalMin", -500, 500).step(this.settings.step);
-    this.horizontalMaxCtrl = this.gui.add(this.settings, "horizontalMax", -500, 500).step(this.settings.step);
-    this.stepCtrl = this.gui.add(this.settings, "step", 0, 10).step(1);
+    verticalMinCtrl = gui.add(settings, "verticalMin", -500, 500).step(step);
+    verticalMaxCtrl = gui.add(settings, "verticalMax", -500, 500).step(step);
+    horizontalMinCtrl = gui.add(settings, "horizontalMin", -500, 500).step(step);
+    horizontalMaxCtrl = gui.add(settings, "horizontalMax", -500, 500).step(step);
+    stepCtrl = gui.add(settings, "step", 0, 10).step(1);
   }
 
-  AnimationApp.prototype.animate = function() {
-    requestAnimationFrame(function() {
-      return this.animate();
-    }.bind(this));
-    this.render();
+  function animate() {
+    requestAnimationFrame(animate);
+    render();
   }
 
-  AnimationApp.prototype.render = function() {
-    this.theta = Math.round((Date.now() - this.time) / 100);
+  function render() {
+    theta = Math.round((Date.now() - time) / 100);
 
     // Animate this.camera based on mouse position
-    this.camera.position.x += ( this.mouseX - this.camera.position.x ) * .05;
-    this.camera.position.y += ( - this.mouseY + 200 - this.camera.position.y ) * .05;
-    this.camera.lookAt( this.scene.position );
+    camera.position.x += ( mouseX - camera.position.x ) * .05;
+    camera.position.y += ( - mouseY + 200 - camera.position.y ) * .05;
+    camera.lookAt( scene.position );
 
     // Clear this.scene on every render
-    this.clearScene();
+    clearScene();
     // Recreate lines in new positions
-    for (var i = this.settings.verticalMin; i < this.settings.verticalMax; i++) {
-      this.createLine(new THREE.Vector3(0, i * 10, 0), i, this.theta);
+    for (i = 0; i < 15; i++) {
+      createLine(new THREE.Vector3(0, i * 10, 0), i, theta);
     }
 
-    if(this.theta === 10) {
+    if(theta === 10) {
 
     }
     //
@@ -128,45 +132,68 @@
     //   };
     // }
 
-    this.renderer.render(this.scene, this.camera);
+    renderer.render(scene, camera);
   }
 
-  AnimationApp.prototype.clearScene = function() {
-    for(var i = this.scene.children.length - 1; i >= 0; i--) {
-      if(this.scene.children[i] instanceof THREE.Mesh) {
-        this.scene.remove(this.scene.children[i]);
+  function clearScene() {
+    for(j = scene.children.length - 1; j >= 0; j--) {
+      if(scene.children[j] instanceof THREE.Mesh) {
+        scene.remove(scene.children[j]);
       }
     }
   }
 
-  AnimationApp.prototype.onDocumentMouseMove = function( event ) {
-    this.mouseX = event.clientX - this.windowHalfX;
-    this.mouseY = event.clientY - this.windowHalfY;
+  function onDocumentMouseMove(event) {
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
   }
 
-  AnimationApp.prototype.createLine = function(position, index, variation) {
+  function createLine(position, index, variation) {
     var geometry = new THREE.Geometry();
     var index, pointPos;
 
-    var points = this.createPoints(this.settings.horizontalMin, this.settings.horizontalMax, this.settings.step, index, variation);
+    var points = createPoints(index);
     var curve = new THREE.CatmullRomCurve3(points);
 
-    for(var i = 0; i < points.length * this.settings.lineSubdivs; i++) {
-      index = i / (points.length * this.settings.lineSubdivs);
+    for(k = 0; k < points.length * lineSubdivs; k++) {
+      index = k / (points.length * lineSubdivs);
       pointPos = curve.getPoint(index);
 
-      geometry.vertices[i] = new THREE.Vector3(pointPos.x, pointPos.y, pointPos.z);
+      geometry.vertices[k] = new THREE.Vector3(pointPos.x, pointPos.y, pointPos.z);
     }
 
+    // createMeshLine(position, geometry);
+
+    createBasicLine(position, geometry);
+  }
+
+  function createBasicLine(position, geometry) {
+    var material = new THREE.LineBasicMaterial({
+      color: 0xffffff,
+      opacity: 1,
+      linewidth: 3
+    });
+
+    var line = new THREE.Line(geometry, material);
+    line.position.x = position.x;
+    line.position.y = position.y;
+    line.position.z = position.z;
+
+    line.name = 'line';
+
+    scene.add(line);
+  }
+
+  function createMeshLine(position, geometry) {
     var line = new MeshLine();
     line.setGeometry(geometry);
 
     var material = new MeshLineMaterial({
       color: new THREE.Color(0xffffff),
-      resolution: this.resolution,
+      resolution: resolution,
       lineWidth: 2,
-      near: this.camera.near,
-      far: this.camera.far,
+      near: camera.near,
+      far: camera.far,
       depthWrite: false,
       dashArray: new THREE.Vector2(10, 5),
       side: THREE.DoubleSide,
@@ -180,41 +207,40 @@
 
     meshline.name = 'line';
 
-    this.scene.add( meshline );
+    scene.add( meshline );
   }
 
-  AnimationApp.prototype.createPoints = function(min, max, step, index, variation) {
+  function createPoints(index) {
     var pointsArr = [];
 
-    for(var i = min, j = max; i<=j; i+=step) {
-      if(variation) {
-        pointsArr.push(new THREE.Vector3(i, this.noise.perlin2(i / max * 10, index / 10) * 10, 0));
-      } else {
-        pointsArr.push(new THREE.Vector3(i, 0, 0));
-      }
+    for(l = -500; l<=500; l+=10) {
+      pointsArr.push(new THREE.Vector3(l, noise.perlin2(l / 500 * 10, index / 10) * 10, 0));
     }
 
     return pointsArr;
   }
 
-  AnimationApp.prototype.addEvents = function() {
-    document.addEventListener( 'mousemove', function(e) {
-      return this.onDocumentMouseMove(e);
-    }.bind(this), false );
+  function addEvents() {
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
     document.querySelector('.finish-btn').addEventListener('click', function(e) {
       console.log('clicked');
     });
-  }
 
-  new AnimationApp({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    verticalMin: -15,
-    verticalMax: 15,
-    horizontalMin: -500,
-    horizontalMax: 500,
-    step: 1,
-    lineSubdivs: 6
-  });
+    // verticalMinCtrl.onFinishChange(function(value) {
+    //   verticalMin = value;
+    // });
+    // verticalMaxCtrl.onFinishChange(function(value) {
+    //   verticalMax = value;
+    // });
+    // horizontalMinCtrl.onFinishChange(function(value) {
+    //   horizontalMin = value;
+    // });
+    // horizontalMaxCtrl.onFinishChange(function(value) {
+    //   horizontalMax = value;
+    // });
+    // stepCtrl.onFinishChange(function(value) {
+    //   step = value;
+    // });
+  }
 })();
